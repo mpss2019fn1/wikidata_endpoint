4420e73a4add17d65d3048c80d15b195b28e9769
Julian Weise <privat@julianweise.de>
2019-05-06 10:11:05 +0200

    add imports in python.ini

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from wikidata_endpoint import WikidataEndpoint
+from wikidata_request_executor import WikidataRequestExecutor
+from wikidata_endpoint_configuration import WikidataEndpointConfiguration
