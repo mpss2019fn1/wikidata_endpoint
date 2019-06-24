60a66a190aa86c46d043ed155e4bfe49527b118c
Alexander Riese <alexander.riese@student.hpi.de>
2019-06-24 16:33:43 +0200

    Add unit tests for endpoint
    Co-authored-by: Hung Nguyen <hungn@outlook.de>

---
+++ tests/configuration_test.py
@@ -0,0 +1,17 @@
+from wikidata_endpoint_configuration import WikidataEndpointConfiguration
+from pytest import fixture
+from pathlib import Path
+
+
+@fixture
+def endpoint_configuration():
+    return WikidataEndpointConfiguration(Path("tests/test_config.ini"))
+
+
+def test_value_reading(endpoint_configuration):
+    assert endpoint_configuration.remote_url() == 'http://test-url.com'
+    assert endpoint_configuration.concurrent_requests() == 42
+    assert endpoint_configuration.request_timeout() == 43
+    assert endpoint_configuration.with_return_type() == 0
+
+
